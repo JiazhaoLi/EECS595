f81c14e25845194a521468914f2bf71a85626c2f
Jiazhao Li <jiazhaoli@Jiazhaos-MacBook-Pro.local>
2018-10-23 19:27:07 -0400

    some data split for training and test

---
+++ main_train.py
@@ -0,0 +1,5 @@
+from preprocess import *
+
+#data_convert_csv()
+# data_sample_csv()
+# split_feature_label()
--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,60 @@
+data_path = '../data/'
+file_name = 'yelp_academic_dataset_review.json'
+data_export = '../data/data_split/'
 
+import json
+from tqdm import tqdm
+import pandas as pd
+import ast
+import csv 
+import numpy as np
 
+def data_convert_csv():
+    with open(data_path + file_name) as json_file:      
+        data = json_file.readlines()  # list of string  
+    # store the data in csv file 
+    with open(data_path+ 'yelp_academic_dataset_review.csv', 'w') as f:
+        # write the heading 
+        review_dict = ast.literal_eval(data[0])
+        heading = review_dict.keys()
+        writer = csv.writer(f)
+        writer.writerow(list(heading))
 
-filename = './yelp_academic_dataset_review.json'
-with open(filename, 'r') as f:
-    review_list = []
-    for review in f:
-        review_list.append(review)
-print(len(review_list))
+        for index in tqdm(range(len(data))):
+            review_dict = json.loads(data[index])
+            writer = csv.writer(f)
+            writer.writerow(review_dict.values())
+
+def data_sample_csv():
+    with open(data_path + file_name) as json_file:      
+        data = json_file.readlines()  # list of string 
+    print("finished json file load") 
+    # store the data in csv file 
+    with open(data_path+ 'sample_yelp_academic_dataset_review.csv', 'w') as f:
+        # write the heading 
+        review_dict = ast.literal_eval(data[0])
+        heading = review_dict.keys()
+        writer = csv.writer(f)
+        writer.writerow(list(heading))
+        for index in tqdm(range(len(data[:100000]))):
+            review_dict = json.loads(data[index])
+            writer = csv.writer(f)
+            writer.writerow(review_dict.values())
+    
+def split_feature_label():
+    df = pd.read_csv(data_path + 'sample_yelp_academic_dataset_review.csv',low_memory=False)
+    print('finished load data')
+
+    print(len(df['stars']))
+
+    with open(data_export + 'train/sample_train.csv','w') as f:
+        for i in df['text']:
+            f.write(i + '\n')
+    
+    with open(data_export + 'test/sample_test.csv','w') as f:
+        for i in df['stars']:
+            f.write(str(i))
+            f.write('\n')
+
+    
+    
